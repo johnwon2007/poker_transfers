acc30bafb925c4cee001905e1ed5b4af28f0cdc5
John Won <johnwon2007@gmail.com>
2025-01-16 02:17:14 +0900

    removed main

--- backend_service/ledger_to_transfer.py
+++ backend_service/ledger_to_transfer.py
@@ -5,5 +5,3 @@ def ledger_tranfer_calculator(file_path):
     balances, id_nick_net = pc(file_path)
     transactions = cmt(balances)
     return transactions, id_nick_net
-if __name__ == "__main__":
-    ledger_tranfer_calculator('/Users/johnwon/Desktop/Projects/poker_money/ledger_service/ledger_test.csv')
